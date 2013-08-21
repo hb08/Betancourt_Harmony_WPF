dd43e085d978426abf5ddaf4e29f001864752264
hb08 <fated.decree@gmail.com>
2013-08-21 19:42:16 -0400

    Conditionals Industry
    Added some tested variables

--- Conditionals_Industry/js/Test.js
+++ Conditionals_Industry/js/Test.js
@@ -1,14 +1,9 @@
 
 
 
-//Known Variables
-if (siteStyle === "Simple" || siteStyle === "simple") {
-    var pageCost = 20;
-} else if (siteStyle === "Professional" || siteStyle === "professional") {
-    var pageCost = 35;
-} else if (siteStyle === "Fun" || siteStyle === "fun") {
-    var pageCost = 30;
-} else {
-    var pageCost = 25;
-}
+
+
+
+
+
 
--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -27,36 +27,66 @@ if (siteSell === "Yes") {
         //set siteCart to 0
     var siteCart = "0";
 }
-    //Is a blog needed?
+//Is a blog needed?
 var siteBlog = prompt("Will your site have a blog?\nYes or No?");
-    //Is a forum needed?
+//Is a forum needed?
 var siteForum = prompt("Will your site have a forum?\nYes or No?");
-    //Is a CMS needed?
+//Is a CMS needed?
 var siteCms = prompt("Would you like a Content Management System (CMS)?\nYes or No?");
-    //Does the customer have graphics?
+//Does the customer have graphics?
 var siteGraphics = prompt("Do you have graphics you would like on the site?\nYes or No?");
-    //If yes
+//If yes
 if (siteGraphics === "Yes"){
-        //How many do they have?
+    //How many do they have?
     siteUserGraphics = prompt("How many graphics do you have to upload?\nWhole numbers only, please.");
-        //If no, set siteUserGraphics to 0
+    //If no, set siteUserGraphics to 0
 } else {
-        //If no, set SiteUserGraphics to 0
+    //If no, set SiteUserGraphics to 0
     siteUserGraphics = 0;
 }
-    //Does the customer need graphics?
+//Does the customer need graphics?
 var siteNewGraphics = prompt("Do you have graphics you would like us to provide?\nYes or No?")
-    //If yes
+//If yes
 if (siteNewGraphics === "Yes"){
-        //Prompt for how many
+    //Prompt for how many
     siteFindGraphics = prompt("How many graphics would you like us to provide?\nWhole numbers only, please.");
     //If no
 } else {
-        //set to 0
+    //set to 0
     siteFindGraphics = 0;
 }
-
+// Do they want a responsive design?
 var siteMobile = prompt("Do you want a mobile accessible site?\nYes or No?");
+
+//Known Variables
+
+// Flat cost for site by page (before any fluff)
+if (siteStyle === "Simple" || siteStyle === "simple") {
+    var pageCost = 20;
+} else if (siteStyle === "Professional" || siteStyle === "professional") {
+    var pageCost = 35;
+} else if (siteStyle === "Fun" || siteStyle === "fun") {
+    var pageCost = 30;
+} else {
+    var pageCost = 25;
+}
+
+// Cost of items/shopping cart
+//Item Scale = <100 = 10 cents, 100 - 500 = 20 cents, 500+ = 30 cents
+var itemScale = [.1,.2,.3]
+if (siteItems === 0) {
+    var pageItemCost = siteItems
+}
+else if (siteItems <= 100 && siteItems >0) {
+    var pageItemCost = siteItems * itemScale[0];
+} else if (siteItems > 100 && siteItems <= 500) {
+    var pageItemCost = siteItems *  itemScale[1];
+} else {
+    var pageItemCost = siteItems * itemScale[2];
+}
+var pageShopping;
+(siteShopping === "Yes") ?  pageShopping = 50 :  pageShopping = 0;
+
 
 
 
