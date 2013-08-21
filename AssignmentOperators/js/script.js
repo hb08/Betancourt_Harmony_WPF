d7e05df710f5682566e70086a964257c55756442
hb08 <fated.decree@gmail.com>
2013-08-20 20:16:34 -0400

    AssignmentOperators With Notation

--- AssignmentOperators/js/script.js
+++ AssignmentOperators/js/script.js
@@ -1,7 +1,11 @@
 // JavaScript
 
+//Name variables X and Y, any numerical value should work
 var x = 2;
 var y = 3;
 
+// use assignment operator to add y to x without writing full equation
 var y += x;
+
+//print y to console
 console.log(y);
