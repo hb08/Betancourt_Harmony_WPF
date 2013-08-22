f001a65cd2998579cd978b72f1b257647899d3b1
hb08 <fated.decree@gmail.com>
2013-08-21 21:52:26 -0400

    Conditionals Wacky
    Changed my mind on what to do, since I don't know how to do everything
    I want.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,2 +1,2 @@
-// Title: Heroclix Team Assessment  Author: Harmony Betancourt  Date: 8/21/13
-// Purpose: Allows user to enter a Heroclix Team option and calculate total cost
+// Title: Astrology  Author: Harmony Betancourt  Date: 8/21/13
+// Purpose: User inputs birth date, and receives Western and Eastern Astrology reading
