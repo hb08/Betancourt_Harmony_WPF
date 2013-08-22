f29774a8e14cedadf70dbd1a779edccb42937d50
hb08 <fated.decree@gmail.com>
2013-08-21 23:10:37 -0400

    Conditionals Wacky - Givens from User Prompts

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,2 +1,7 @@
 // Title: Astrology  Author: Harmony Betancourt  Date: 8/21/13
 // Purpose: User inputs birth date, and receives Western and Eastern Astrology reading
+
+//User givens via prompts
+var birthMonth = prompt("What month were you born?\nPlease give month number.")  ;
+var birthDate = prompt("What is day were you born?\nNumbers only, please.") ;
+var birthYear = prompt("What year were you born?");
