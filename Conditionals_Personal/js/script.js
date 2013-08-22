1e5f6c1fff2f7ee81115965cb04e2121132fb943
hb08 <fated.decree@gmail.com>
2013-08-22 16:28:08 -0400

    Conditionals Personal
    Beginning notation

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,4 @@
+// Title: Mutants and Masterminds 2nd Ed. Character Calculator  Author: Harmony Betancourt  Date: 8/22/13
+// Purpose: Calculates point total for entire character and compares PP level
+// Note: If this works, use for Claremont
+
