1fc642ffb2556073753130a2db68598ec3767d6a
hb08 <fated.decree@gmail.com>
2013-08-26 23:48:05 -0400

    Functions_Worksheet
    Annonymous Function Start

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,8 +1,11 @@
 // Project: Functions Worksheet  Author: Harmony Betancourt  Date: 8/26/11
 
 // Radius of a Circle
-
-
+//Anonymous Function
+var calCircum = function(d) {
+    var circum = d / 2;
+    return circum;
+}
 // Bee Stings
 
 
