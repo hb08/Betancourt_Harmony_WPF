0b10b2d9a0d00451267ff77cec3826f912516cea
hb08 <fated.decree@gmail.com>
2013-08-26 23:50:06 -0400

    Functions Worksheet
    + Prompt and Argument

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -6,6 +6,11 @@ var calCircum = function(d) {
     var circum = d / 2;
     return circum;
 }
+//Prompt
+var diameter = prompt("What is the diameter of the circle?");
+//Arguments
+var totalCircum = calCircum (5);
+
 // Bee Stings
 
 
